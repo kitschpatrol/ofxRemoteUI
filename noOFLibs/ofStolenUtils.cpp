52ada8685437e5e7a6ac1e8ac2c976de8667f506
Eric Mika <eric@ericmika.com>
2016-05-19 15:16:21 -0400

    Fixes for compilation without Cinder's precompiled header.

--- noOFLibs/ofStolenUtils.cpp
+++ noOFLibs/ofStolenUtils.cpp
@@ -1,5 +1,6 @@
 #include "ofStolenUtils.h"
-#include <string.h>
+#include <cfloat>
+#include <math.h>
 
 int ofToInt(const std::string &intString) {
 	int x = 0;
@@ -21,23 +22,25 @@ float ofClamp(float value, float min, float max) {
 	return value < min ? min : value > max ? max : value;
 }
 
-//check for division by zero???
+// check for division by zero???
 //--------------------------------------------------
 float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax, bool clamp) {
 	if (fabs(inputMin - inputMax) < FLT_EPSILON) {
 		return outputMin;
-	}
-	else {
+	} else {
 		float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
 
 		if (clamp) {
 			if (outputMax < outputMin) {
-				if (outVal < outputMax)outVal = outputMax;
-				else if (outVal > outputMin)outVal = outputMin;
-			}
-			else {
-				if (outVal > outputMax)outVal = outputMax;
-				else if (outVal < outputMin)outVal = outputMin;
+				if (outVal < outputMax)
+					outVal = outputMax;
+				else if (outVal > outputMin)
+					outVal = outputMin;
+			} else {
+				if (outVal > outputMax)
+					outVal = outputMax;
+				else if (outVal < outputMin)
+					outVal = outputMin;
 			}
 		}
 		return outVal;
